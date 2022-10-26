6d9d4cf26fa1decbb7d23cddcbe47b0c6bb7f1ad
erik <erikrm02@gmail.com>
2022-10-26 17:54:10 +0200

    last update

--- controller/crearController.php
+++ controller/crearController.php
@@ -1,18 +1,17 @@
 <?php
 //recoge los datos de crear vista y los llevamos a alumno que es dnd los creamos, y vuelve
 
-// require_once "../models/alumno.php";
+require_once "../model/alumno.php";
+
+$nombre = $_POST['name'];
+$edad = $_POST['edad'];
+
+Alumno::crearAlumno($nombre, $edad);
 
 
-// $nombre = $_POST['name'];
-// $edad = $_POST['edad'];
 
+echo "<script>  
 
-// Alumno::crearAlumno($nombre, $edad);
+location.href='./indexcontroller.php';
 
-
-// echo "<script>  
-
-// location.href='./indexcontroller.php';
-
-// </script>";
+</script>";
