7ead275fab47ac23bc87d30b3471931da4094cab
SergioMerino2021 <100006618.joan23@fje.edu>
2022-10-28 17:32:07 +0200

    fix table

--- controller/connection.php
+++ controller/connection.php
@@ -1,8 +1,8 @@
 <?php
 
-$server = 'localhost';
-$username = 'root';
-$password = '';
+$server = '192.168.24.109';
+$username = 'admin1';
+$password = 'admin1234';
 $bd = 'bd_sintesis';
 // Nos conectamos a la base de datos mediante la funcion mysqli_connect
 $conexion = mysqli_connect($server,$username,$password,$bd);
