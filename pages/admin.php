32583af1e6c4c5cdb420c8f76dcd6ee62fa87351
edgarmarcoss <fifacallput2002@gmail.com>
2022-11-03 23:48:53 +0100

    Last update

--- pages/admin.php
+++ pages/admin.php
@@ -1,4 +1,5 @@
 <?php
+  error_reporting(0);
   session_start();
 
   if(empty($_SESSION['login'])){
@@ -15,6 +16,7 @@
   $listaAlumno=$lista[0];
   $total_pages=$lista[1];
   $page=$lista[2];
+
   
 ?>
     
@@ -46,23 +48,6 @@
           <li><a class="link_name" href="../pages/formulario.php">Crear Alumno</a></li>
         </ul>
       </li>
-
-      <!-- <li>
-        <div class="iocn-link">
-          <a href="#">
-            <i class='bx bx-collection' ></i>
-            <span class="link_name">Category</span>
-          </a>
-          <i class='bx bxs-chevron-down arrow' ></i>
-        </div>
-        <ul class="sub-menu">
-          <li><a class="link_name" href="#">Category</a></li>
-          <li><a href="#">HTML & CSS</a></li>
-          <li><a href="#">JavaScript</a></li>
-          <li><a href="#">PHP & MySQL</a></li>
-        </ul>
-      </li> -->
-      
       <li>
       <li>
         <a href="./correo.php">
@@ -84,9 +69,7 @@
         </ul>
       </li>
 
-      
-
-      
+          
       <li>
         <div class="profile-details">
           <div class="profile-content">
@@ -110,43 +93,6 @@
       <i class='bx bx-menu' ></i>
       <span class="text">HOME</span>
     </div>
-
-    <!-- <nav class="navbar navbar-expand-lg bg-light">
-    <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
-          </a>
-          <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
-          </ul>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link disabled">Disabled</a>
-        </li>
-      </ul>
-      <form class="d-flex" role="search">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-success" type="submit">Search</button>
-      </form>
-    </div>
-  </div>
-</nav> -->
 
     <div class="tabla" style="overflow: scroll;height:80vh">
       <table class="table" style="text-align:center ;">
