9ff44b6ce1856bbcc405540f7f1742e04a54b414
edgarmarcoss <fifacallput2002@gmail.com>
2022-11-03 23:50:21 +0100

    Last update

--- js/filtrado.js
+++ js/filtrado.js
@@ -1,15 +1,17 @@
 const filtro =()=>{
+    //Recogemos los elementos
     var matricula = document.getElementById('matricula');
     var nombre = document.getElementById('nombre');
     var ape = document.getElementById('apellido');
     var ape2 = document.getElementById('apellido2');
     var correo = document.getElementById('correo');
     var dni = document.getElementById('dni');
-    //alert(matricula.value);
+    //Enviar a la funcion redirect con el valor del elemento
     redirect(nombre.value,ape.value,ape2.value,correo.value,dni.value,matricula.value)
 
 }
 
 const redirect = (nombre,ape,ape2,correo,dni,matricula)=>{
+    //Redirigimos la url con los elementos
     location.href='../pages/admin.php?nombre='+nombre+'&apellido='+ape+'&apellido2='+ape2+'&correo='+correo+'&dni='+dni+'&matricula='+matricula;
 }
