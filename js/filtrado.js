5707bc323e5e86e2c27c21f1d9a3b0b35368b0f8
edgarmarcoss <fifacallput2002@gmail.com>
2022-11-03 22:18:06 +0100

    Last update

--- js/filtrado.js
+++ js/filtrado.js
@@ -1,15 +1,15 @@
 const filtro =()=>{
-    //var matricula = document.getElementById('matricula');
+    var matricula = document.getElementById('matricula');
     var nombre = document.getElementById('nombre');
-    // var ape = document.getElementById('apellido');
-    // var ape2 = document.getElementById('appelido2');
-    // var correo = document.getElementById('correo');
-    // var dni = document.getElementById('dni');
+    var ape = document.getElementById('apellido');
+    var ape2 = document.getElementById('apellido2');
+    var correo = document.getElementById('correo');
+    var dni = document.getElementById('dni');
     //alert(matricula.value);
-    redirect(nombre.value)
+    redirect(nombre.value,ape.value,ape2.value,correo.value,dni.value,matricula.value)
 
 }
 
-const redirect = (nombre)=>{
-    location.href='../pages/admin.php?nombre='+nombre;
+const redirect = (nombre,ape,ape2,correo,dni,matricula)=>{
+    location.href='../pages/admin.php?nombre='+nombre+'&apellido='+ape+'&apellido2='+ape2+'&correo='+correo+'&dni='+dni+'&matricula='+matricula;
 }
