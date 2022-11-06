bf14dc3ca16e57e9addf487197c1b08547f1a55a
SergioMerino2021 <100006618.joan23@fje.edu>
2022-11-06 10:34:00 +0100

    info

--- pages/info.php
+++ pages/info.php
@@ -3,19 +3,27 @@
     require_once '../components/cabecera.html';
 
 ?>
+<link rel="stylesheet" href="../css/info.css"/>
 <body>
     
 
-  
-<nav class="navbar bg-dark">
+
+<!-- <nav class="navbar bg-dark">
   <div class="container-fluid">
     <img src="../img/logo.png" alt="" width="50" height="24" class="d-inline-block align-text-top"></a>
+
+  </div>
+</nav> -->
+
+<div class="fondo">
+  <div class="container">
+    <h1>App de Notas</h1>
     <form class="d-flex" role="search" action="../pages/login.php" method="POST">
       <button class="btn btn-outline-success" name="login" type="submit">Log In</button>
     </form>
   </div>
-</nav>
 
+</div>
 
 </body>
 </html>
